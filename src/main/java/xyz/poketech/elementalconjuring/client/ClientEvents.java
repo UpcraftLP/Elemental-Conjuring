d433b661587a1c62d49eb662ee5f5d69c4a9a985
UpcraftLP <j-schirra@gmx.de>
2018-01-06 04:31:33 +0100

    updated jump handler

--- src/main/java/xyz/poketech/elementalconjuring/client/ClientEvents.java
+++ src/main/java/xyz/poketech/elementalconjuring/client/ClientEvents.java
@@ -2,11 +2,13 @@ package xyz.poketech.elementalconjuring.client;
 
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import xyz.poketech.elementalconjuring.ElementalConjuring;
+import xyz.poketech.elementalconjuring.handler.JumpHandler;
 import xyz.poketech.elementalconjuring.net.NetworkHandler;
 import xyz.poketech.elementalconjuring.net.packet.PacketJump;
 
@@ -18,12 +20,18 @@ import xyz.poketech.elementalconjuring.net.packet.PacketJump;
 public class ClientEvents {
 
     private static final Minecraft mc = Minecraft.getMinecraft();
+    private static long lastJump = 0;
 
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void onPlayerPressSpace(InputEvent.KeyInputEvent event) {
         if(mc.player == null) return;
-        if(mc.gameSettings.keyBindJump.isPressed() && mc.player.isAirBorne) {
-            NetworkHandler.INSTANCE.sendToServer(new PacketJump());
+        if(mc.gameSettings.keyBindJump.isPressed() && mc.player.isAirBorne && !mc.player.isCreative()) {
+            long current = System.nanoTime() / 1000L;
+            if(current - lastJump > JumpHandler.JUMP_DELAY) { //reduce unnecessary network overhead
+                NetworkHandler.INSTANCE.sendToServer(new PacketJump());
+                lastJump = current;
+            }
+
         }
     }
 
--- src/main/java/xyz/poketech/elementalconjuring/handler/JumpHandler.java
+++ src/main/java/xyz/poketech/elementalconjuring/handler/JumpHandler.java
@@ -24,6 +24,8 @@ public class JumpHandler implements IMessageHandler<PacketJump, IMessage> {
 
     private static final Map<UUID, Long> JUMP_PLAYERS = Maps.newHashMap();
     private static final Map<UUID, Boolean> SIDES = Maps.newHashMap();
+    public static final long JUMP_DELAY = 300L;
+    public static final int COLOR = new Color(0x29535D).getRGB();
 
     @SuppressWarnings("unused")
     public JumpHandler() {
@@ -32,36 +34,40 @@ public class JumpHandler implements IMessageHandler<PacketJump, IMessage> {
 
     @Override
     public IMessage onMessage(PacketJump message, MessageContext ctx) {
-        EntityPlayer player = ctx.getServerHandler().player;
-        UUID playerUUID = player.getUniqueID();
-        long current = System.nanoTime() / 1000L;
-        long lastJump = JUMP_PLAYERS.getOrDefault(playerUUID, 0L);
-        if(current - lastJump > 150) {  //TODO check if player has capability
-            if(!player.onGround) {
-                //TODO wall jumps should bounce off the wall instead of just this
-                Vec3d lookVec = player.getLookVec();
-                int toggle = SIDES.getOrDefault(playerUUID, false) ? 1 : -1;
-                player.motionX += lookVec.z * 0.3F * toggle;
-                player.motionZ += lookVec.x * 0.3F * toggle;
-                SIDES.put(playerUUID, toggle == -1);
+        synchronized (JUMP_PLAYERS) {
+            EntityPlayer player = ctx.getServerHandler().player;
+            if(!player.isCreative()) {
+                UUID playerUUID = player.getUniqueID();
+                long current = System.nanoTime() / 1000L;
+                long lastJump = JUMP_PLAYERS.getOrDefault(playerUUID, 0L);
+                if(current - lastJump > JUMP_DELAY) {  //TODO check if player has capability
+                    if(!player.onGround) {
+                        //TODO wall jumps should bounce off the wall instead of just this
+                        Vec3d lookVec = player.getLookVec();
+                        int toggle = SIDES.getOrDefault(playerUUID, false) ? 1 : -1;
+                        player.motionX += lookVec.z * 0.3F * toggle;
+                        player.motionZ += lookVec.x * 0.3F * toggle;
+                        SIDES.put(playerUUID, toggle == -1);
 
-                EntitySummonedItem entity = new EntitySummonedItem(player.world);
-                entity.setPositionAndRotation(player.posX, player.posY, player.posZ, player.rotationYaw, player.rotationPitch); //FIXME orient circle properly!
-                entity.setColor(new Color(0x9DB6C7).getRGB());
-                entity.lifeSpan = 70;
-                entity.ticksExisted +=20;
-                player.world.spawnEntity(entity);
+                        EntitySummonedItem entity = new EntitySummonedItem(player.world, COLOR);
+                        entity.setPositionAndRotation(player.posX, player.posY, player.posZ, player.rotationYaw, player.rotationPitch); //FIXME orient circle properly!
+                        entity.lifeSpan = 150;
+                        entity.setNoPickup(true);
+                        entity.ticksExisted = EntitySummonedItem.SPAWN_ANIMATION_TIME * 3;
+                        player.world.spawnEntity(entity);
 
-                float f1 = player.rotationYaw * 0.017453292F;
-                player.motionY += 0.42F * 2;
-                player.motionX -= (double)(MathHelper.sin(f1) * 0.4F);
-                player.motionZ += (double)(MathHelper.cos(f1) * 0.4F);
+                        float f1 = player.rotationYaw * 0.017453292F;
+                        player.motionY += 0.42F * 2;
+                        player.motionX -= (double)(MathHelper.sin(f1) * 0.4F);
+                        player.motionZ += (double)(MathHelper.cos(f1) * 0.4F);
 
-                player.fallDistance = 0;
-                player.velocityChanged = true;
+                        player.fallDistance = 0;
+                        player.velocityChanged = true;
+                    }
+                }
+                JUMP_PLAYERS.put(playerUUID, current);
             }
         }
-        JUMP_PLAYERS.put(playerUUID, current);
         return null; //we don't need to return a message to the client
     }
 }
